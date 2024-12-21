23a8d83d3d6f424ac8a629687a1a0e2e456eaaa9
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-12-21 17:32:18 +0530

    added support for "weights' parameter

--- main.go
+++ main.go
@@ -20,8 +20,9 @@ func acceptableLine(line string, line_no int) bool {
 	}
 
 	for _, message := range messages {
-		role, roleOk := message["role"].(string)
-		content, contentOk := message["content"].(string)
+		role, roleOk := message["role"].(string)          // required
+		content, contentOk := message["content"].(string) // required
+		weight, weightOk := message["weight"].(uint8)     // optional
 		if !roleOk {
 			fmt.Printf("Line %d: Missing required field \"role\"", line_no)
 			return false
@@ -29,6 +30,12 @@ func acceptableLine(line string, line_no int) bool {
 		if !contentOk {
 			fmt.Printf("Line %d: Missing required field \"content\"", line_no)
 			return false
+		}
+		if weightOk {
+			if weight != 0 && weight != 1 {
+				fmt.Printf("Line %d: Invalid \"weight\" %d found\n", line_no, weight)
+				return false
+			}
 		}
 		if role != "assistant" && role != "system" && role != "user" {
 			fmt.Printf("Line %d: Invalid \"role\" %s found\n", line_no, role)
